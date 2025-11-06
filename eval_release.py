979995dc572d50a8ba6b43ef57e509844d43fdfa
david-basil <david.basil@hotmail.com>
2025-11-06 11:44:16 -0700

    evaluation changes using tsv golds

--- eval_release.py
+++ eval_release.py
@@ -19,6 +19,11 @@ def file_to_pairs(f):
                 seen.add(pair)
     return pairs
 
+def file_to_set(f):
+    with open(f, 'r') as fh:
+        all_tokens = [a.strip() for a in fh.readlines()]
+    return set(all_tokens)
+
 
 
 # Read in the list of synsets to cover, and their gold contents.
@@ -42,6 +47,8 @@ with open(file_gold, 'r') as f:
 # Read the senses to be evaluated (into a list of pairs).
 senses_for_eval = file_to_pairs(file_eval) 
 
+core_synsets = file_to_set('dependencies/corebnout.txt')
+
 
 # Get counts and report.
 num_synsets_in_gold = len(gold_bnid_to_lemmas.keys()) # denom. for synset recall
@@ -60,20 +67,27 @@ for bnid in gold_bnid_to_lemmas:
 
 correct_senses = 0 # num. for sense eval
 synsets_with_correct_sense = set()
+synsets_present_in_output = set()
 for (bnid, lemma) in senses_for_eval:
   if bnid in gold_bnid_to_lemmas and lemma in gold_bnid_to_lemmas[bnid]:
     print("GOOD_SENSE",bnid,lemma,sep='\t')
     correct_senses += 1
     synsets_with_correct_sense.add(bnid)
+    synsets_present_in_output.add(bnid)
   else:
     print("BAD_SENSE",bnid,lemma,sep='\t')
+    synsets_present_in_output.add(bnid)
 num_synsets_with_correct_sense = len(synsets_with_correct_sense) # num. for synset eval
+
+
 
 
 def safe_div(n, d):
   return n / d if d > 0 else 0.0
 
 print()
+
+
 
 ### SENSE-LEVEL EVALUATION
 sense_precision = safe_div(correct_senses, num_senses_for_eval)
@@ -95,12 +109,16 @@ synset_precision = safe_div(num_synsets_with_correct_sense, num_senses_covered)
 synset_recall    = safe_div(num_synsets_with_correct_sense, num_synsets_in_gold)
 synset_f1 = safe_div( (2 * synset_precision * synset_recall), 
                       (synset_precision + synset_recall) )
+core_coverage = safe_div(len(synsets_present_in_output & core_synsets),  len(core_synsets))
+
+
 
 print(f"SYNSET\tnum_synsets_with_correct_sense: {num_synsets_with_correct_sense}")
 print(f"SYNSET\tnum_senses_covered:    {num_senses_covered}")
 print(f"SYNSET\tnum_synsets_in_gold:   {num_synsets_in_gold}")
-print('SYNSET', 'PRECISION', round(100*synset_precision, 1), sep='\t')
-print('SYNSET', 'RECALL   ', round(100*synset_recall,    1), sep='\t')
-print('SYNSET', 'F1       ', round(100*synset_f1,        1), sep='\t')
+print('SYNSET', 'PRECISION    ', round(100*synset_precision, 1), sep='\t')
+print('SYNSET', 'RECALL       ', round(100*synset_recall,    1), sep='\t')
+print('SYNSET', 'F1           ', round(100*synset_f1,        1), sep='\t')
+print('SYNSET', 'CORE COVERAGE', round(100*core_coverage,        1), sep='\t')
 print()
 
